602d633238271a75711271f1de796e5e2a619b85
carlod035 <carlosdf035@gmail.com>
2024-01-11 22:21:59 +0000

    Delete layout.js, globals.css, and page.js files, and update dependencies and tailwind.config.js

---
+++ components/Nav.jsx
@@ -0,0 +1,25 @@
+'use client'
+import Image from 'next/image'
+import Link from 'next/link';
+
+
+
+
+export const Nav = () => {
+  return (
+    <div className="flex itens-center justify-between p-[50px]">
+      <Image
+      className='cursor-pointer'
+        src="/logo.png"
+        alt="logo"
+        width={150}
+        height={100}
+      />
+      <div className='flex gap-[15px] items-center'>
+       <Link href="/profile"> <p className='text-xl font-mono text-white hover:text-neutral-300 cursor-pointer'>PROFILE</p>
+       </Link>
+        <p className='text-xl font-mono text-white hover:text-neutral-300 cursor-pointer'>CONTACTS</p>
+      </div>
+    </div>
+  )
+}
